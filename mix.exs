c596b13ade1c34f3bcad082a1668fbfecc642888
Max Veytsman <mveytsman@github.com>
2021-03-06 16:59:53 -0500

    Add licenses and links to mix.exs

--- mix.exs
+++ mix.exs
@@ -8,12 +8,11 @@ defmodule HeroiconsElixir.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-
-      # Docs
-      name: "Heroicons Elixir",
+      name: "Heroicons",
       source_url: "https://github.com/mveytsman/heroicons_elixir",
       description: "Include Heroicons as SVG-strings in your Elixir/Phoenix project!",
-      docs: docs()
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -30,6 +29,13 @@ defmodule HeroiconsElixir.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ex_doc, "~> 0.23", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mveytsman/heroicons_elixir"}
     ]
   end
 
