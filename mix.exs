4e43e7323a81342d9ee61dd272d7d9841f125767
Max Veytsman <mveytsman@github.com>
2021-03-10 13:51:48 -0500

    Bump patch

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HeroiconsElixir.MixProject do
   def project do
     [
       app: :heroicons,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
