c180227ad7b786384219dc665137ce2c179ed6b9
Max Veytsman <maxim@ontoillogical.com>
2021-09-09 00:21:51 -0400

    Bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HeroiconsElixir.MixProject do
   def project do
     [
       app: :heroicons,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
